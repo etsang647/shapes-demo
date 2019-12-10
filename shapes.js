9aa97c504b923eba8b80c563eb9281da970d399f
Eric Tsang <etsang647@gmail.com>
2019-12-09 23:54:58 -0500

    comments and cleanup

--- shapes.js
+++ shapes.js
@@ -1,3 +1,6 @@
+// Eric Tsang
+
+// essential variables/constants
 canvas = document.getElementById("map");
 context = canvas.getContext("2d");
 
@@ -14,33 +17,33 @@ const lowerMapHeight = 2 * canvasHeight / 3;
 const x_vel = 10;
 const y_vel = 10;
 const platform_vel = 5;
-var gravity_vel = 5;
+var gravity_vel = 5; // not const since it is affected by acceleration
 
+// 3 rectangles - the "characters" interacting in this demonstration
 rectLeft = {
     x_pos: canvasWidth / 5,
     y_pos: upperMapHeight
 };
-
 rectCenter = {
     x_pos: canvasWidth / 2,
     y_pos: lowerMapHeight,
 };
-
 rectRight = {
     x_pos: 3 * canvasWidth / 4,
     y_pos: lowerMapHeight
 };
 
+// platform that carries rectCenter from lowerMapHeight to upperMapHeight
 rectPlatform = {
     x_pos: canvasWidth / 3,
     y_pos: lowerMapHeight + rectHeight
 }
-
+// buttons controlled by rectLeft and rectRight that lower and raise
+// rectPlatform respectively
 rectButtonLeft = {
     x_pos: 0,
     y_pos: upperMapHeight + rectHeight
 }
-
 rectButtonRight = {
     x_pos: canvasWidth - rectWidth,
     y_pos: lowerMapHeight + rectHeight
@@ -69,19 +72,21 @@ function moveRect(position, xOffset, yOffset) {
             break;
     }
 }
-
+// check if x_pos + xOffset is within horizontal bounds of canvas and map
 function withinXBounds(x_pos, xOffset) {
     if (x_pos + xOffset < 0 || x_pos + xOffset > canvasWidth)
         return false
     return true
 }
 
+// check if y_pos + yOffset is within vertical bounds of canvas and map
 function withinYBounds(y_pos, yOffset) {
     if (y_pos + yOffset < 0 || y_pos + yOffset > lowerMapHeight)
         return false
     return true
 }
 
+// lowers/raises rectPlatform by yOffset (if positive/negative respectively)
 function movePlatform(yOffset) {
     if (withinYBounds(rectPlatform.y_pos - rectHeight, yOffset)) {
         rectPlatform.y_pos += yOffset;
@@ -90,6 +95,7 @@ function movePlatform(yOffset) {
     }
 }
 
+// buggy collision detection: checks if rectCenter is on the rectPlatform
 function onPlatform() {
     if (Math.abs(rectCenter.x_pos - rectPlatform.x_pos) < rectWidth && rectCenter.y_pos + rectHeight <= rectPlatform.y_pos + platformHeight) {
         return true
@@ -97,18 +103,21 @@ function onPlatform() {
     return false
 }
 
+// checks if rectLeft is on the left button
 function onLeftButton() {
     if (Math.abs(rectLeft.x_pos - rectButtonLeft.x_pos) < rectWidth) {
         return true
     }
 }
 
+// checks if rectRight is on the right button
 function onRightButton() {
     if (Math.abs(rectRight.x_pos - rectButtonRight.x_pos) < rectWidth) {
         return true
     }
 }
 
+// gravity acceleration (messy implementation)
 function gravity() {
     if (rectCenter.x_pos < canvasWidth / 3 && rectCenter.y_pos < upperMapHeight) {
         gravity_vel += 0.2;
@@ -127,6 +136,8 @@ function gravity() {
     }
 }
 
+// checks if rectCenter is on the upper side of the map in which case it should
+// smile in the loop() function
 function isSmiling() {
     if (rectCenter.x_pos < canvasWidth / 3 && rectCenter.y_pos <= upperMapHeight + y_vel) {
         return true
@@ -135,21 +146,25 @@ function isSmiling() {
 }
 
 function loop() {
-    let circleXPos = rectCenter.x_pos + rectWidth / 2;
-    let circleYPos = rectCenter.y_pos + rectHeight / 2;
+    // fill entire canvas
     context.fillStyle = "#7D7ABC";
     context.fillRect(0, 0, canvasWidth, canvasHeight)
 
+    // fill map structure
     context.fillStyle = "#6457A6";
     context.fillRect(0, upperMapHeight + rectHeight, canvasWidth / 3, canvasHeight - upperMapHeight - rectHeight)
     context.fillRect(canvasWidth / 3, lowerMapHeight + rectHeight, 2 * canvasWidth / 3, lowerMapHeight)
 
+    // fill left rect
     context.fillStyle = "#23F0C7";
     context.fillRect(rectLeft.x_pos, rectLeft.y_pos, rectWidth, rectHeight);
 
+    // fill center rect
     context.fillStyle = "#FFE347";
     context.fillRect(rectCenter.x_pos, rectCenter.y_pos, rectWidth, rectHeight);
-    context.moveTo(rectCenter.x_pos, rectCenter.y_pos);
+    // adds smile or frown to center rect depending on position
+    let circleXPos = rectCenter.x_pos + rectWidth / 2;
+    let circleYPos = rectCenter.y_pos + rectHeight / 2;
     context.beginPath();
     if (isSmiling())
         context.arc(circleXPos, circleYPos, 30, 0, Math.PI, false);
@@ -161,16 +176,20 @@ function loop() {
     context.arc(circleXPos + 15, circleYPos - 10, 5, 0, Math.PI * 2, true);
     context.stroke();
 
+    // fill right rect
     context.fillStyle = "#23F0C7";
     context.fillRect(rectRight.x_pos, rectRight.y_pos, rectWidth, rectHeight);
 
+    // fill platform
     context.fillStyle = "#FFE347";
     context.fillRect(rectPlatform.x_pos, rectPlatform.y_pos, rectWidth, platformHeight);
 
+    // fill left and right buttons
     context.fillStyle = "#23F0C7";
     context.fillRect(rectButtonLeft.x_pos, rectButtonLeft.y_pos, rectWidth, platformHeight);
     context.fillRect(rectButtonRight.x_pos, rectButtonRight.y_pos, rectWidth, platformHeight);
 
+    // handle platform movement and gravity depending on rectCenter position
     if (onLeftButton()) {
         movePlatform(platform_vel)
     }
@@ -181,23 +200,28 @@ function loop() {
         gravity();
     }
 
+    // call loop function again to draw next frame
     window.requestAnimationFrame(loop);
 }
 
+// keyboard navigation for the 3 rectangles
 window.addEventListener("keydown", function (event) {
     switch (event.code) {
+        // A and D for left and right movement of rectLeft
         case "KeyA":
             moveRect("left", -x_vel, 0)
             break;
         case "KeyD":
             moveRect("left", x_vel, 0)
             break;
+        // left and right arrow keys for left and right movement of rectCenter
         case "ArrowLeft":
             moveRect("center", -x_vel, 0)
             break;
         case "ArrowRight":
             moveRect("center", x_vel, 0)
             break;
+        // J and L for left and right movement of rectRight
         case "KeyJ":
             moveRect("right", -x_vel, 0)
             break;
@@ -207,4 +231,5 @@ window.addEventListener("keydown", function (event) {
     }
 }, true);
 
+// begin demo loop
 window.requestAnimationFrame(loop);
